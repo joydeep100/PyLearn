617c0346e1dd337d9de620b03e4a9673f049b35d
joydeep100 <joydeep56053@gmail.com>
2020-04-09 12:17:59 +0530

    added code

--- basic.py
+++ basic.py
@@ -1,6 +1,34 @@
-from itertools import combinations
-s , n  = raw_input().split()
+#//Converting a list back to string.
+string = "abracadabra"
+l = list(string)
+l[5] = 'k'
+string = ''.join(l)
 
-for i in range(1, int(n)+1):
-    for j in combinations(sorted(s), i):
-        print(''.join(j))
+
+#//Replace
+'Hello world'.replace('l','z')
+#O/P 'Hezzo worzd'
+
+#Sorted <object>.sort() changes the original string, but sorted is like an filter that is applied over
+min = sorted(list(set([i[1] for i in scores_list])))[1]
+
+# Justify  - center, ljust & rjust
+# >>> 'hello'.center(10,'-')
+# '--hello---'
+# >>> 'hello'.center(10,'-')
+# '--hello---'
+
+#//Working with reverse slicing, A better approach
+print([1,2,3,4,5][1:3])
+#O/P [2, 3]
+print([1,2,3,4,5][1:3][::-1])
+#O/P [3, 2]
+
+#Extend vs Appned in list
+l=[]
+l.append([1])
+print(l)
+# O/P [[1]]
+l.extend([2])
+print(l)
+# O/P [[1], 2]
