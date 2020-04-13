b51ea2c13335e4b67b484663b9e07cfb1f0e12e6
joydeep100 <joydeep56053@gmail.com>
2020-04-14 00:20:09 +0530

    added code

--- inheritance2.py
+++ inheritance2.py
@@ -1,7 +1,5 @@
 class Employee:
-
     raise_amt = 1.04
-
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
@@ -14,19 +12,15 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amt)
 
-
 class Developer(Employee):
     raise_amt = 1.10
-
     def __init__(self, first, last, pay, prog_lang):
         super().__init__(first, last, pay)
         self.prog_lang = prog_lang
 
-
 class Manager(Employee):
-
     def __init__(self, first, last, pay, employees=None):
-        super().__init__(first, last, pay)
+        super().__init__(first, last, pay) # or Employee.__init__(self,first, last, pay)
         if employees is None:
             self.employees = []
         else:
