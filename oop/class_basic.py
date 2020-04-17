9305b4596fbbc42ab14be9e7593b8628ac92eeaf
joydeep100 <joydeep56053@gmail.com>
2020-04-17 12:30:49 +0530

    added new code

---
+++ oop/class_basic.py
@@ -0,0 +1,4 @@
+# Prviate variables
+# Please note that private variables (vars starting with '_' )don't exist in Python. 
+# There are simply norms to be followed. The language itself don't apply any restrictions.
+# These are generally used in cojuction with property, see property example
