c069cc6a39b4618fd953350f0290aceab79e3279
joydeep100 <joydeep56053@gmail.com>
2020-05-11 13:57:19 +0530

    added code

--- dsa/recursion/fibo.py
+++ dsa/recursion/fibo.py
@@ -1,11 +1,22 @@
 def fibo(n):
-	if n == 0:
-		return 0
-	elif n == 1:
-		return 1
+	if n == 0 or n == 1:
+		return n
 	else:
 		return fibo(n-1) + fibo(n-2) 
 
-import sys
-n = int(sys.argv[1])
-print(fibo(n))
+D={}
+def fiboUsingDP(n):
+	if n == 0 or n ==1:
+		return n
+
+	elif n in D.keys():
+		return D[n]
+	
+	else:
+		result = fiboUsingDP(n-1) + fiboUsingDP(n-2)
+		D[n]=result		#Caching all intermediate results (Dynamic Programming)
+		return result
+
+n=40
+# print(fibo(n))
+print(fiboUsingDP(n))
