a2c5efdb143f992265ba47ff605dbeca65b426f5
joydeep100 <joydeep56053@gmail.com>
2020-05-29 14:54:58 +0530

    minor refactor

--- packing.py
+++ packing.py
@@ -1,6 +1,5 @@
 # list <-> tuple
 def packing(*args):
-	# print(type(args))
 	print(args)
 
 def unpack(a,b,c,d):
@@ -10,14 +9,13 @@ def unpack(a,b,c,d):
 packing(1,2,3,4,5)
 unpack(*[1,2,3,4])
 
-# dictionary
+# named arguments <-> dictionary
 def packing_dict(**args):
-	# print(type(args))
 	print(args)
 
 def unpack_dict(a,b,c):
 	print(a,b,c)
 
-packing_dict(a=1,b='strong',c=3)	#no quotes for keys
+packing_dict(a=1,b='strong',c=3)	#Named arguments
 unpack_dict(**{'a':1,'b':2,'c':3})
 
